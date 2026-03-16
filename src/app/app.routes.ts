e98e2f61cc60269e7ff6347c8ab17430390ff60a
starlo <saoend27@gmail.com>
2026-03-16 16:41:59 +0800

    Revert "Merge branch 'JiaYu' into starlo"
    This reverts commit 3ec5acb3fcf5aad5307b6f4b9562d670a9996656, reversing
    changes made to 5ac1bbaa26da10e34fe88ec6af56681b4fd37bb2.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,29 +1,3 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { VisitorComponent } from './pages/visitor/visitor.component';
-import { AnnouncementComponent } from './pages/announcement/announcement.component';
-import { BillComponent } from './pages/bill/bill.component';
-import { FacilityComponent } from './pages/facility/facility.component';
-import { PackageComponent } from './pages/package/package.component';
-import { RepairComponent } from './pages/repair/repair.component';
-import { LayoutComponent } from './shared/layout/layout.component';
 
-export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  {
-    path: '',
-    component: LayoutComponent,
-    children: [
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'visitor', component: VisitorComponent },
-      { path: 'announcement', component: AnnouncementComponent },
-      { path: 'bill', component: BillComponent },
-      { path: 'facility', component: FacilityComponent },
-      { path: 'package', component: PackageComponent },
-      { path: 'repair', component: RepairComponent },
-      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-    ]
-  },
-  { path: '**', redirectTo: 'dashboard' }
-];
+export const routes: Routes = [];
