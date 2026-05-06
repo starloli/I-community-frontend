214a46f017d987a36872b9f75315a7287832571d
8fgxmtqf2y-del <8fgxmtqf2y@privaterelay.appleid.com>
2026-05-06 15:54:54 +0800

    收款成功Toast通知

--- src/app/@service/auth.service.ts
+++ src/app/@service/auth.service.ts
@@ -5,7 +5,7 @@ import { catchError, Observable, tap, throwError } from 'rxjs';
 import { ToastService } from './toast.service';
 import { VerifyCodeType } from '../interface/enum';
 import { updateUser } from '../interface/interface';
-import { ToastService } from './toast.service';
+
 
 @Injectable({
   providedIn: 'root'
