601cd6726d157c94fd2d79e3f0cb9a07029167f6
starlo <saoend27@gmail.com>
2026-05-06 15:50:55 +0800

    1

--- src/app/@service/auth.service.ts
+++ src/app/@service/auth.service.ts
@@ -2,7 +2,6 @@ import { HttpClient, HttpResponse } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { catchError, Observable, tap, throwError } from 'rxjs';
-import { ToastService } from './toast.service';
 import { VerifyCodeType } from '../interface/enum';
 import { updateUser } from '../interface/interface';
 import { ToastService } from './toast.service';
