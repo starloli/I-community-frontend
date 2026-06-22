ed0cebf81b20101f9514db7817456a9aaf9bbf15
starlo <saoend27@gmail.com>
2026-06-22 23:38:47 +0800

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://backend-production-097b8.up.railway.app' // User needs to change this
+  baseUrl: 'https://i-community-backstage.onrender.com' // User needs to change this
 };
